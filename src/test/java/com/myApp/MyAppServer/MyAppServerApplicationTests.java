939edadf6cd6f2f1e835535ede79a4d284f6cd8f
handaehee93 <hdh8990@naver.com>
2024-09-15 15:41:39 +0900

    프로젝트 생성

---
+++ src/test/java/com/myApp/MyAppServer/MyAppServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myApp.MyAppServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyAppServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
